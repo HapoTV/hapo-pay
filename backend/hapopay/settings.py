cb9a25363158ac041e66fcf704c6cb9afcbd42a6
njogubless <pnjogubless@gmail.com>
2026-07-06 18:14:11 +0300

    feat(accounts): adding simpleJWT.token_blacklist in the apps

--- backend/hapopay/settings.py
+++ backend/hapopay/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     # Third party apps
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'channels',
     'drf_yasg',
