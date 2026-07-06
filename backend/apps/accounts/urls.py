3c3ab374e480beb2cd6cbace634dfd32554b3763
njogubless <pnjogubless@gmail.com>
2026-07-06 18:17:30 +0300

    feat(urls): registering the route for Token Refresh

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('token/refresh/', views.TokenRefreshView.as_view(), name='token-refresh'),
     path('change-password/', views.ChangePasswordView.as_view(), name='change-password'),
     path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot-password'),
     path('reset-password/', views.ResetPasswordView.as_view(), name='reset-password'),
